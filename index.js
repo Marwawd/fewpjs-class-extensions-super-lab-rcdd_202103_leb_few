d9b0d7aa85823fde8522ae68011dc770e77d5b44
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 22:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Evergreen extends Tree {
   constructor(species,name){
     super(species);
     this.name=name;
-    
+
   }
   static definition (){
     return (super(definition)+"Evergreens keep their leaves all year round.");
